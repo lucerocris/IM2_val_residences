c45c0681c88c0aa9a3e8162e56cdc0c16ffe3398
Sai <gementizasgg08@gmail.com>
2025-06-20 11:00:01 +0800

    feat and refactor: add about us main content + created components for content text in AboutUs and landing

--- resources/js/pages/main/AboutUs.tsx
+++ resources/js/pages/main/AboutUs.tsx
@@ -7,8 +7,57 @@ const landing = () => {
     const title = "About Us";
 
     return (
-        <MainLayout carouselText = {<CarouselText description = {desc} buttonLabel= {btnLabel} title = {title} />}/>
+        <MainLayout carouselText = {<CarouselText description = {desc} buttonLabel= {btnLabel} title = {title} />}>
+            <div className = "p-[4.3rem] flex gap-[40px] items-start">
+                <div className = "flex-[2] text-[1.1em] leading-[1.5]">
+                    <Text />
+                </div>
+
+                <div className = "flex-[1]">
+                    <div className = "grid grid-cols-2 gap-2.5">
+                        <img src="https://t4.ftcdn.net/jpg/00/76/25/75/360_F_76257590_OMqEbhnSnz30cLj6xAG511xSZrJabcsq.jpg" alt="" />
+                        <img src="https://t4.ftcdn.net/jpg/00/76/25/75/360_F_76257590_OMqEbhnSnz30cLj6xAG511xSZrJabcsq.jpg" alt="" />
+
+                        <img src="https://t4.ftcdn.net/jpg/00/76/25/75/360_F_76257590_OMqEbhnSnz30cLj6xAG511xSZrJabcsq.jpg" alt="" />
+                        <img src="https://t4.ftcdn.net/jpg/00/76/25/75/360_F_76257590_OMqEbhnSnz30cLj6xAG511xSZrJabcsq.jpg" alt="" />
+
+                        <img src="https://t4.ftcdn.net/jpg/00/76/25/75/360_F_76257590_OMqEbhnSnz30cLj6xAG511xSZrJabcsq.jpg" alt="" />
+                        <img src="https://t4.ftcdn.net/jpg/00/76/25/75/360_F_76257590_OMqEbhnSnz30cLj6xAG511xSZrJabcsq.jpg" alt="" />
+
+                        <img src="https://t4.ftcdn.net/jpg/00/76/25/75/360_F_76257590_OMqEbhnSnz30cLj6xAG511xSZrJabcsq.jpg" alt="" />
+                        <img src="https://t4.ftcdn.net/jpg/00/76/25/75/360_F_76257590_OMqEbhnSnz30cLj6xAG511xSZrJabcsq.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </MainLayout>
     )
+}
+
+const Text = () => {
+    return(
+        <>
+            <h2 className = "mb-[15px] text-3xl font-bold">Welcome to Val Residences</h2>
+            <p className = "mb-[40px]">Your home in the exclusive seaside community of Corona del Mar in Talisay City, Cebu. Nestled within
+                    this Spanish Mediterranean-inspired beachfront subdivision, our apartments offer the perfect blend
+                    of resort-style living and modern city convenience.</p>
+            <p className = "mb-[40px]">Just a 15-minute drive from Cebu City via the South Coastal Road, Corona del Mar is the first
+                    residential beachfront development of its kind in Cebu. Here, you'll wake up to breathtaking
+                    panoramic views, a refreshing sea breeze, and the serenity of living close to nature—while still
+                    enjoying easy access to schools, businesses, and shopping centers.</p>
+            <h3 className = "mb-[15px] text-2xl font-bold">Features & Amenities</h3>
+            <ul className = "pl-[20px] list-disc">
+                <li>Beach Frontage facing Bohol Strait</li>
+                <li>Well-lighted Spine Road with Tree Lines</li>
+                <li>Clubhouse</li>
+                <li>Infinity Pool with Shower Rooms</li>
+                <li>View Tower</li>
+                <li>Gazebo</li>
+                <li>Tennis and Basketball Courts</li>
+                <li>Landscaped Open Area</li>
+                <li>Pocket Parks</li>
+            </ul>
+        </>
+    );
 }
 
 export default landing;
--- resources/js/pages/main/landing.tsx
+++ resources/js/pages/main/landing.tsx
@@ -15,80 +15,46 @@ const landing = () => {
                     {/* Boxes here */}
 
                     <div className = "w-full max-w-[350px] h-[450px] bg-[#e8e8e8] text-left m-auto">
-                        <div className = "h-[40%] overflow-hidden">
-                            <img src="https://media.istockphoto.com/id/2155879397/photo/house-in-a-charming-neighborhood-with-stunning-sidewalk-landscaping.jpg?s=612x612&w=0&k=20&c=nQJOUoNb2RNev3QVNjIohcmxQABCTetCdgfnc8MV8sU=" alt="pic" className = "w-full h-full object-cover"/>
-                        </div>
-                        <div className = "p-[20px] h-[41%]">
-                            <h3 className = "text-[1.2rem] mb-[10px] font-bold">Ph. 4, Lot 6, Block 8</h3>
-                            <p className = "mt-[5px] text-[0.8rem]">Corona del Mar, Pooc, Talisay City, Cebu</p>
-                            <br />
-                            <p className = "mt-[5px] text-[0.8rem]">Duplex</p>
-                            <p className = "mt-[5px] text-[0.8rem]">Vacant Units: 0</p>
-                            <p className = "mt-[5px] text-[0.8rem]">PHP 11,000.00 - PHP 20,000.00</p>
-                        </div>
-                        <div className = "h-[19%] w-full overflow-hidden flex flex-row justify-evenly items-center">
-                            <Btn className = "px-[33px] h-[35px]"><Map/></Btn>
-                            <Btn className = "px-[42px] h-[35px]">VIEW UNITS</Btn>
-                        </div>
+                        <CardContent />
                     </div>
 
                      <div className = "w-full max-w-[350px] h-[450px] bg-[#e8e8e8] text-left m-auto">
-                        <div className = "h-[40%] overflow-hidden">
-                            <img src="https://media.istockphoto.com/id/2155879397/photo/house-in-a-charming-neighborhood-with-stunning-sidewalk-landscaping.jpg?s=612x612&w=0&k=20&c=nQJOUoNb2RNev3QVNjIohcmxQABCTetCdgfnc8MV8sU=" alt="pic" className = "w-full h-full object-cover"/>
-                        </div>
-                        <div className = "p-[20px] h-[41%]">
-                            <h3 className = "text-[1.2rem] mb-[10px] font-bold">Ph. 4, Lot 6, Block 8</h3>
-                            <p className = "mt-[5px] text-[0.8rem]">Corona del Mar, Pooc, Talisay City, Cebu</p>
-                            <br />
-                            <p className = "mt-[5px] text-[0.8rem]">Duplex</p>
-                            <p className = "mt-[5px] text-[0.8rem]">Vacant Units: 0</p>
-                            <p className = "mt-[5px] text-[0.8rem]">PHP 11,000.00 - PHP 20,000.00</p>
-                        </div>
-                        <div className = "h-[19%] w-full overflow-hidden flex flex-row justify-evenly items-center">
-                            <Btn className = "px-[33px] h-[35px]"><Map/></Btn>
-                            <Btn className = "px-[42px] h-[35px]">VIEW UNITS</Btn>
-                        </div>
+                        <CardContent />
                     </div>
 
                     <div className = "w-full max-w-[350px] h-[450px] bg-[#e8e8e8] text-left m-auto">
-                        <div className = "h-[40%] overflow-hidden">
-                            <img src="https://media.istockphoto.com/id/2155879397/photo/house-in-a-charming-neighborhood-with-stunning-sidewalk-landscaping.jpg?s=612x612&w=0&k=20&c=nQJOUoNb2RNev3QVNjIohcmxQABCTetCdgfnc8MV8sU=" alt="pic" className = "w-full h-full object-cover"/>
-                        </div>
-                        <div className = "p-[20px] h-[41%]">
-                            <h3 className = "text-[1.2rem] mb-[10px] font-bold">Ph. 4, Lot 6, Block 8</h3>
-                            <p className = "mt-[5px] text-[0.8rem]">Corona del Mar, Pooc, Talisay City, Cebu</p>
-                            <br />
-                            <p className = "mt-[5px] text-[0.8rem]">Duplex</p>
-                            <p className = "mt-[5px] text-[0.8rem]">Vacant Units: 0</p>
-                            <p className = "mt-[5px] text-[0.8rem]">PHP 11,000.00 - PHP 20,000.00</p>
-                        </div>
-                        <div className = "h-[19%] w-full overflow-hidden flex flex-row justify-evenly items-center">
-                            <Btn className = "px-[33px] h-[35px]"><Map/></Btn>
-                            <Btn className = "px-[42px] h-[35px]">VIEW UNITS</Btn>
-                        </div>
+                        <CardContent />
                     </div>
 
                     <div className = "w-full max-w-[350px] h-[450px] bg-[#e8e8e8] text-left m-auto">
-                        <div className = "h-[40%] overflow-hidden">
-                            <img src="https://media.istockphoto.com/id/2155879397/photo/house-in-a-charming-neighborhood-with-stunning-sidewalk-landscaping.jpg?s=612x612&w=0&k=20&c=nQJOUoNb2RNev3QVNjIohcmxQABCTetCdgfnc8MV8sU=" alt="pic" className = "w-full h-full object-cover"/>
-                        </div>
-                        <div className = "p-[20px] h-[41%]">
-                            <h3 className = "text-[1.2rem] mb-[10px] font-bold">Ph. 4, Lot 6, Block 8</h3>
-                            <p className = "mt-[5px] text-[0.8rem]">Corona del Mar, Pooc, Talisay City, Cebu</p>
-                            <br />
-                            <p className = "mt-[5px] text-[0.8rem]">Duplex</p>
-                            <p className = "mt-[5px] text-[0.8rem]">Vacant Units: 0</p>
-                            <p className = "mt-[5px] text-[0.8rem]">PHP 11,000.00 - PHP 20,000.00</p>
-                        </div>
-                        <div className = "h-[19%] w-full overflow-hidden flex flex-row justify-evenly items-center">
-                            <Btn className = "px-[33px] h-[35px]"><Map/></Btn>
-                            <Btn className = "px-[42px] h-[35px]">VIEW UNITS</Btn>
-                        </div>
+                        <CardContent />
                     </div>
                 </div>
             </div>    
         </MainLayout>
     )
+}
+
+const CardContent = () => {
+    return(
+        <>
+            <div className = "h-[40%] overflow-hidden">
+                <img src="https://media.istockphoto.com/id/2155879397/photo/house-in-a-charming-neighborhood-with-stunning-sidewalk-landscaping.jpg?s=612x612&w=0&k=20&c=nQJOUoNb2RNev3QVNjIohcmxQABCTetCdgfnc8MV8sU=" alt="pic" className = "w-full h-full object-cover"/>
+            </div>
+            <div className = "p-[20px] h-[41%]">
+                <h3 className = "text-[1.2rem] mb-[10px] font-bold">Ph. 4, Lot 6, Block 8</h3>
+                <p className = "mt-[5px] text-[0.8rem]">Corona del Mar, Pooc, Talisay City, Cebu</p>
+                <br />
+                <p className = "mt-[5px] text-[0.8rem]">Duplex</p>
+                <p className = "mt-[5px] text-[0.8rem]">Vacant Units: 0</p>
+                <p className = "mt-[5px] text-[0.8rem]">PHP 11,000.00 - PHP 20,000.00</p>
+            </div>
+            <div className = "h-[19%] w-full overflow-hidden flex flex-row justify-evenly items-center">
+                <Btn className = "px-[33px] h-[35px]"><Map/></Btn>
+                <Btn className = "px-[42px] h-[35px]">VIEW UNITS</Btn>
+            </div>
+        </>
+    );
 }
 
 export default landing;
