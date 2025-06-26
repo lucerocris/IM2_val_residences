d857817598fee266bb532d9537b72b55694bed16
Sai <gementizasgg08@gmail.com>
2025-06-26 14:43:05 +0800

    refactor: made new controller for User

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\MainSection;
 use App\Http\Controllers\TenantController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', [MainSection::class, 'home']);
 Route::get('/about', [MainSection::class, 'about']);
@@ -11,3 +12,5 @@ Route::get('/contact', [MainSection::class, 'contact']);
 
 Route::get('/tenant', [TenantController::class, 'index']);
 Route::get('/tenant/listings', [TenantController::class, 'listings']);
+
+Route::get('/user',[UserController::class, 'index']);
