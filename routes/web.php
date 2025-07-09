a92703b75874b841d196ab483962d803652f3d63
actuallynotDeru <crazydean2005@gmail.com>
2025-07-09 21:42:58 +0800

    fix: Merge Conflict

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,15 @@ Route::middleware('auth')->group(function () {
 Route::middleware('auth', 'user.type:tenant')->group(function () {
     Route::get('/tenant/dashboard', [TenantController::class, 'index']);
     Route::get('/tenant/listings', [TenantController::class, 'listings']);
+
+// Tenant Maintenance Request
     Route::post('/tenant/maintenanceRequest', [RequestMaintenanceController::class, 'store']);
+
+
+// Tenant Payment Upload
+    Route::get('/tenant/payments/gcash', [TenantController::class, 'gcash']);
+    Route::get('/tenant/payments/paymaya', [TenantController::class, 'paymaya']);
+    Route::get('/tenant/payments/bankTransfer', [TenantController::class, 'bank']);
 });
 
 //Prospective Routes
