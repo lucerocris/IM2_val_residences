04dd9759aa5d47392541e3860d2a4c0544cc4da6
actuallynotDeru <crazydean2005@gmail.com>
2025-07-16 00:05:05 +0800

    Fix: Hard coded prospective_tenant_id to match test prospective tenant id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,7 +28,6 @@ class UserController extends Controller
 
     public function applications() {
         $Applications = RentalApplication::getOwnApplication(Auth::id());
-        dd(Auth::id());
         return Inertia::render('user/Applications', [
             'ApplicationData' => $Applications,
         ]);
