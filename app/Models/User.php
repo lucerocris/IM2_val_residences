09b9e6dbe5db3495a2b608808dc5a2bdd2275392
Cris Lucero <lucerocris22@gmail.com>
2025-07-18 10:03:06 +0800

    feat: added soft delete in tenant (related lease's status == terminated, and related unit == available)

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Nanigans\SingleTableInheritance\SingleTableInheritanceTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -28,6 +29,7 @@ class User extends Authenticatable implements CanResetPassword
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use SingleTableInheritanceTrait;
+    use SoftDeletes;
     use HasFactory, Notifiable, CanResetPasswordTrait;
     protected $table = 'users';
 
